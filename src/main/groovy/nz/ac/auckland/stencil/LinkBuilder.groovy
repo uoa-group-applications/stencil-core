2e2abdb1db58261354b34f55b31e654fc0c8beb7
Irina Benediktovich <i.benediktovich@auckland.ac.nz>
2013-09-09 12:49:29 +1200

    Added LinkBuilder

---
+++ src/main/groovy/nz/ac/auckland/stencil/LinkBuilder.groovy
@@ -0,0 +1,112 @@
+package nz.ac.auckland.stencil
+
+import nz.ac.auckland.common.stereotypes.UniversityComponent
+import nz.ac.auckland.stencil.Path
+import nz.ac.auckland.stencil.Stencil
+
+import javax.inject.Inject
+
+/**
+ * Author: Marnix
+ *
+ * Builds links to stencils with certain parameter maps
+ */
+@UniversityComponent
+class LinkBuilder {
+
+	/**
+	 * default context path if nothing is defined
+	 */
+	private static final String DEFAULT_CONTEXTPATH = '/'
+
+	/**
+	 * name of the property
+	 */
+	private static final String WEBAPP_CONTEXT_PROPERTY = 'webapp.context'
+
+	/**
+	 * cached context path
+	 */
+	private String contextPath
+
+	/**
+	 * @return the web applications context path
+	 */
+	public String getContextPath() {
+		if (contextPath == null)
+			contextPath = System.getProperty(WEBAPP_CONTEXT_PROPERTY, DEFAULT_CONTEXTPATH);
+
+		return contextPath
+	}
+
+	/**
+	 * Create a simple link
+	 *
+	 * @param url is the URL to link to
+	 * @return a link that includes the context path
+	 */
+	public String linkTo(String url) {
+
+		if (url == null) {
+			throw new IllegalArgumentException('Should pass URL argument');
+		}
+
+		// always should start with a /
+		if (!url.startsWith('/')) {
+			url = "/$url";
+		}
+
+		StringBuilder strBuilder = new StringBuilder()
+		strBuilder.append(getContextPath()).append(url);
+		return strBuilder.toString()
+	}
+
+	/**
+	 * Build a link
+	 *
+	 * @param stencilClass is the class to be linking to
+	 * @param pathParameters are the path parameters to fill out for the link
+	 * @return the URL that is generated
+	 */
+	public String linkTo(Class<? extends Stencil> stencilClass, Map<String, String> pathParameters = [:]) {
+
+		if (!stencilClass) {
+			throw new IllegalArgumentException('Expected stencilclass to be a proper class');
+		}
+
+		if (pathParameters == null) {
+			throw new IllegalArgumentException('Should pass a proper map');
+		}
+
+		Path pathAnnotation = stencilClass.getAnnotation(Path);
+
+		if (!pathAnnotation) {
+			return null;
+		}
+
+		// replace all matchable elements
+		String matcher = pathAnnotation.value();
+		pathParameters.each { String key, String replaceWith ->
+			matcher = matcher.replace("{$key}", replaceWith);
+		}
+
+		if (matcher.indexOf("{") > -1) {
+			String sub = matcher.substring(matcher.indexOf("{"))
+			if (sub.indexOf("}") > -1){
+				sub = sub.substring(0, sub.indexOf("}")+1)
+			} else{
+				sub = sub.take(10)
+			}
+			throw new IllegalArgumentException("Unable to match element $sub");
+		}
+
+		StringBuilder strBuilder = new StringBuilder();
+		strBuilder
+				.append(getContextPath())
+				.append(matcher)
+
+		return strBuilder.toString();
+
+	}
+
+}
---
+++ src/test/groovy/nz/ac/auckland/stencil/LinkBuilderTest.groovy
@@ -0,0 +1,104 @@
+package nz.ac.auckland.stencil
+
+import nz.ac.auckland.stencil.Path
+import nz.ac.auckland.stencil.Stencil
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.test.context.ContextConfiguration
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
+
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+/**
+ * Author: Marnix
+ *
+ * Test some of the linkbuilder functionality
+ */
+class LinkBuilderTest {
+
+    /**
+     * Test the link builders simple behavior
+     */
+    @Test
+    public void testSimpleLinkBuilder() {
+        LinkBuilder builder = createLinkBuilder()
+
+        assert builder.linkTo("my-link") == "/context/my-link";
+        assert builder.linkTo("/my-link") == "/context/my-link"
+        assert builder.linkTo("") == "/context/";
+        assert builder.linkTo("/") == "/context/";
+    }
+
+    /**
+     * should throw exception on stupid input
+     */
+    @Test(expected = IllegalArgumentException)
+    public void testIllegalInput() {
+        LinkBuilder builder = createLinkBuilder()
+
+        builder.linkTo((String) null);
+    }
+
+    /**
+     * Happy flow
+     */
+    @Test
+    public void testStencilLinkBuilder() {
+        LinkBuilder builder = createLinkBuilder()
+
+        // exact match
+        String match;
+
+        match = builder.linkTo(TestStencil, [one: "first", two: "second", three: "third"]);
+        assert match == "/context/first/second/content/third";
+    }
+
+    /**
+     * When something can't be matched, it should blow up
+     */
+    @Test(expected = IllegalArgumentException)
+    public void testStencilLinkBuilderWithMissingParameter() {
+        LinkBuilder builder = createLinkBuilder()
+
+        // exact match
+        String match;
+
+        match = builder.linkTo(TestStencil, [one: "first", two: "second", missing: "missing"]);
+    }
+
+    /**
+     * Are there too many parameters? shouldn't matter
+     */
+    public void testWithTooManyParameters() {
+
+        LinkBuilder builder = createLinkBuilder()
+
+        // exact match
+        String match;
+
+        match = builder.linkTo(TestStencil, [one: "first", two: "second", three: "third", missing: "missing"]);
+        assert match == "/context/first/second/content/third";
+    }
+
+    /**
+     * Test stencil
+     */
+    @Path("/{one}/{two}/content/{three}")
+    public static class TestStencil implements Stencil {
+
+        @Override
+        void render(HttpServletRequest request, HttpServletResponse response, Map<String, String> pathParameters) {
+        }
+
+    }
+
+    protected LinkBuilder createLinkBuilder() {
+        LinkBuilder builder = new LinkBuilder();
+	    builder.contextPath = '/context'
+
+
+        return builder
+    }
+
+}
